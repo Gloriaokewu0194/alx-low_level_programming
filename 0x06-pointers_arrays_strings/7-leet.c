8668f6964d9d1986da67f609466b01e3fbaa846b
Gloriaokewu0194 <treasure0194@gmail.com>
2022-12-22 13:07:04 -0800

    7-leet task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,27 @@
 
 /**
  * leet - encodes a string into 1337
- * @n: input
+ * @n: input value
  * Return: the value of n
  */
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "AEOTL";
-	char s2[] = "43071";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j] || n[i] - 32 == s1[j]
+			if (n[i] == s1[j])
+
+			{
 				n[i] = s2[j];
 
+			}
 		}
 	}
 	return (n);
+
 }
